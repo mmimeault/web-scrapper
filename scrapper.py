c6278dbd49b4739c1ca572916120140e1b28d552
Michael Mimeault <michael@ampme.com>
2020-11-24 17:30:19 -0500

    test sucess

--- scrapper.py
+++ scrapper.py
@@ -6,7 +6,7 @@ url = "http://www.premina.ca/puppage.htm"
 page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode('iso-8859-1')
-isBorn = re.search('(born|prêts)', html, re.IGNORECASE)
+isBorn = re.search('(born|prêts|a)', html, re.IGNORECASE)
 if isBorn:
     print("Il y a des bebes!")
     f = open("born.txt", "w")
