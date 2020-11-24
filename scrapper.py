da521a6646bfcce45c4b0c9ec7c177019beccac6
Michael Mimeault <michael@ampme.com>
2020-11-24 17:14:19 -0500

    add exit code

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 # coding=iso-8859-1
 from urllib.request import urlopen
 import re
+import sys
 
 url = "http://www.premina.ca/puppage.htm"
 page = urlopen(url)
@@ -9,6 +10,8 @@ html = html_bytes.decode('iso-8859-1')
 isBorn = re.search('(born|prêts)', html, re.IGNORECASE)
 if isBorn:
     print("Il y a des bebes!")
+    sys.exit(0)
 else:
     print("Faut encore attendre")
+    sys.exit(1)
 
