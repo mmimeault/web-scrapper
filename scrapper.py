caa43f3e4c0cfcd1e025285e96746f822b51adf5
Michael Mimeault <michael@ampme.com>
2020-11-26 12:08:38 -0500

    update script

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,13 @@
 # coding=iso-8859-1
-from urllib.request import urlopen
-import re
 import os
+import re
+from datetime import datetime
+from urllib.request import urlopen
+
+import nexmo
+
+time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(time + ": Started running Scrapper Newegg...")
 
 url = "http://www.premina.ca/puppage.htm"
 page = urlopen(url)
@@ -9,17 +15,13 @@ html_bytes = page.read()
 html = html_bytes.decode('iso-8859-1')
 isBorn = re.search('(born|prêts)', html, re.IGNORECASE)
 
-filename = "output/born.txt"
-os.makedirs(os.path.dirname(filename), exist_ok=True)
-
 if isBorn:
-    print("Il y a des bebes!")
-    f = open(filename, "w")
-    f.write("true")
-    f.close()
+    print(time + ": Il y a des bebes!")
+    client = nexmo.Client(key='2e2108e3', secret='xR7tZzs8bqSEMJJj')
+    client.send_message({
+        'from': '12262471505',
+        'to': '15145548991',
+        'text': 'Hey puppies are ready -> http://www.premina.ca/puppage.htm',
+    })
 else:
-    print("Faut encore attendre")
-    f = open(filename, "w")
-    f.write("false")
-    f.close()
-
+    print(time + ": Faut encore attendre")
--- scrapper_newegg.py
+++ scrapper_newegg.py
@@ -1,13 +1,18 @@
 # coding=iso-8859-1
 import json
+from datetime import datetime
 from urllib.request import urlopen
+
 import nexmo
+
+time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(time + ": Started running Scrapper Newegg...")
 
 products = ["14-932-336", "14-487-518", "14-137-598", "14-137-597",
             "14-487-520", "14-126-452", "14-932-329", "14-126-457",
             "14-126-453", "14-932-330", "14-932-337", "14-487-519",
             "14-487-521", "14-487-522", "14-137-600", "14-932-367",
-                                        "14-133-809", "14-932-345",
+            "14-133-809", "14-932-345",
             "14-137-603", "14-126-459", "14-137-601", "14-487-528",
             "14-932-342", "14-932-359", "14-932-360", "14-137-602",
             "14-487-529", "14-487-531", "14-487-530", "14-137-605",
@@ -29,7 +34,7 @@ for product in products:
         results.append("https://www.newegg.ca/p/pl?d=" + item['Description']['UrlKeywords'])
 
 if results:
-    print("Found an item")
+    print(time + ": Found an item on newegg")
     client = nexmo.Client(key='2e2108e3', secret='xR7tZzs8bqSEMJJj')
     client.send_message({
         'from': '12262471505',
@@ -38,4 +43,4 @@ if results:
     })
 
 else:
-    print("Faut encore attendre")
+    print(time + ": Faut encore attendre pour newegg")
